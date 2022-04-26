ca8080f31c60668712afb3b2e758f9b479c7e11e
Chuan Ji <chuan@jichu4n.com>
2022-04-26 09:08:35 -0700

    Drop SDynamicArray as it is not fully implemented.

--- src/serializable-arrays.ts
+++ src/serializable-arrays.ts
@@ -155,37 +155,6 @@ export abstract class SArrayWithWrapper<
   }
 }
 
-/** An array encoded as a number N followed by N elements. */
-export abstract class SDynamicArray<
-  LengthT extends SerializableWrapper<number>,
-  ValueT extends Serializable = Serializable
-> extends SArray<ValueT> {
-  /** Length type, to be provided by child classes. */
-  protected abstract lengthType: new () => LengthT;
-  /** Element type, to be provided by child classes. */
-  protected abstract valueType: new () => ValueT;
-
-  deserialize(buffer: Buffer, opts?: DeserializeOptions): number {
-    const length = new this.lengthType();
-    let readOffset = length.deserialize(buffer, opts);
-    this.value = times(length.value, () => new this.valueType());
-    readOffset += super.deserialize(buffer.slice(readOffset), opts);
-    return readOffset;
-  }
-
-  serialize(opts?: SerializeOptions) {
-    const length = new this.lengthType();
-    length.value = this.value.length;
-    return Buffer.concat([length.serialize(opts), super.serialize(opts)]);
-  }
-
-  getSerializedLength(opts?: SerializeOptions) {
-    const length = new this.lengthType();
-    length.value = this.value.length;
-    return length.getSerializedLength(opts) + super.getSerializedLength(opts);
-  }
-}
-
 /** Error augmented by SArray with index information. */
 export class SArrayError<
   ValueT extends Serializable = Serializable
