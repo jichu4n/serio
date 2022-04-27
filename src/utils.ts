539d7ed2228c93f2aebb62b775c6d3be2cb8c8b0
Chuan Ji <chuan@jichu4n.com>
2022-04-26 23:46:24 -0700

    Fix handling of null in toJSON.

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
 /** Returns the JSON representation of a value by invoking toJSON() if it exists. */
 export function toJSON(value: any) {
+  if (value === null || value === undefined) {
+    return null;
+  }
   if (typeof value['toJSON'] === 'function') {
     return value.toJSON();
   }
